d5f8d6f7e26bcefde246c160c95362daf2037859
Mateusz <210183@edu.p.lodz.pl>
2020-03-15 14:16:29 +0100

    glass and plots

--- app/configuration/config.py
+++ app/configuration/config.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 
 class Config(object):
-    def __init__(self, dataSourceUrl, test_size, n_splits, should_describe_data, n_data_columns):
+    def __init__(self, dataSourceUrl, test_size, n_splits, should_describe_data):
         self.dataSourceUrl = dataSourceUrl
         self.test_size = test_size
         self.n_splits = n_splits
         self.should_describe_data = should_describe_data
-        self.n_data_columns = n_data_columns
         
 import json
 
@@ -16,7 +15,6 @@ def as_config(dct):
         dct['test_size'],
         dct['n_splits'],
         dct['should_describe_data'],
-        dct['n_data_columns'],
         )
 
 configuration = json.loads(Path("configuration.json").read_text(), object_hook = as_config)
