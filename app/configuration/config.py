bd162285574797bd2265ad80ca49fe116f2bc3ed
Maciej Majchrowski <maciek21197@gmail.com>
2020-03-15 14:21:02 +0100

    after merge woring with glass config

--- app/configuration/config.py
+++ app/configuration/config.py
@@ -7,7 +7,6 @@ class Config(object):
         self.test_size = test_size
         self.n_splits = n_splits
         self.should_describe_data = should_describe_data
-        self.n_data_columns = n_data_columns
 
 def as_config(dict):
     dataSetName = dict['chosenDataSet'][0]
