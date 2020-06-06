8615760c95367d665e5519363b2ee9f2d5e40d5b
Maciej Majchrowski <maciek21197@gmail.com>
2020-06-06 17:23:12 +0200

    feat: t_sne and pca dimensionality reduction

--- jupyter/module-dir/dataset_provider/config.py
+++ jupyter/module-dir/dataset_provider/config.py
@@ -1,13 +1,14 @@
 import json
 
 class Config(object):
-    def __init__(self, dataSourceRaw, dataSourceMapped, useOnlineData, test_size, n_splits, should_describe_data):
+    def __init__(self, dataSourceRaw, dataSourceMapped, useOnlineData, test_size, n_splits, should_describe_data, n_components_pca):
         self.dataSourceRaw = dataSourceRaw
         self.dataSourceMapped = dataSourceMapped
         self.useOnlineData = useOnlineData
         self.test_size = test_size
         self.n_splits = n_splits
         self.should_describe_data = should_describe_data
+        self.n_components_pca = n_components_pca
 
 def as_config(dict, dataSetName):
     return Config(
@@ -17,6 +18,7 @@ def as_config(dict, dataSetName):
         dict[dataSetName]['test_size'],
         dict[dataSetName]['n_splits'],
         dict[dataSetName]['should_describe_data'],
+        dict[dataSetName]['n_components_pca'],
     )
 
 def create_global_config(json_string = """
@@ -27,7 +29,8 @@ def create_global_config(json_string = """
             "useOnlineData": false,
             "test_size": 0.2,
             "n_splits": 10,
-            "should_describe_data": true
+            "should_describe_data": true,
+            "n_components_pca": 10
         }
     }"""):
     return as_config(json.loads(json_string), 'it_data')
